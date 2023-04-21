138be8182ce1a1f0c8c187d1403b2ac6ecf19189
g3th <theinkandpen@yahoo.co.uk>
2023-04-21 17:10:36 +0100

    small fixes

--- Kotlin/cinema.kt
+++ Kotlin/cinema.kt
@@ -1,26 +1,22 @@
 package cinema
 
-fun printCinemaRoom(){
+fun printCinemaRoom(rows: Int, seats: Int){
     println("Cinema:")
-    for (i in 0..8){
+    for (i in 0..seats){
         if (i < 1){
             print ("  ")
         }
-        else if (i == 8){
-            print ("${i}")
+        else if (i == seats){
+            print ("$i")
         }
         else {
-            print ("${i} ")
+            print ("$i ")
         }
     }
-    for (i in 1..7){
+    for (i in 1..rows){
         print("\n${i} ")
-        for (i in 1..8){
+        for (j in 1..seats){
             print("S ")
-            if (i == 7){
-                print("S")
-                break
-            }
         }
     }
 }
@@ -28,7 +24,7 @@ fun printCinemaRoom(){
 fun round(num: Int) : List<Int> {
     var doubleValue : Double = num.toDouble() / 2
     var firstNum : Int
-    val result = mutableListOf<Int>()
+    var result = mutableListOf<Int>()
 
     if (doubleValue % 1 >= 0.5){
         firstNum = (doubleValue + 1).toInt()
@@ -39,14 +35,14 @@ fun round(num: Int) : List<Int> {
 }
 
 fun main() {
-    printCinemaRoom()
-    println("\n\nEnter the number of rows:")
+    println("Enter the number of rows:")
     val rows = readln().toInt()
     println("Enter the number of seats in each row:")
     val seats = readln().toInt()
 
-    val total : Int
+    printCinemaRoom(rows, seats)
 
+    var total = 0
     if (rows * seats <= 60){
         total = 10 * (rows * seats)
     } else {
@@ -54,9 +50,10 @@ fun main() {
             total = (10 * rows / 2 * seats) + (8 * rows / 2 * seats)
         } else {
             val splitOddRows: List<Int> = round(rows)
+            println(splitOddRows[0])
             total = (10 * splitOddRows[1] * seats) + (8 * splitOddRows[0] * seats)
 
         }
     }
-    print("Total income:\n$$total")
+    print("\nTotal income:\n$$total")
 }
